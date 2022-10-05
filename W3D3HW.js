d32e33879a6bde85425ec4d2dbd2fb5b42519093
nicbritz89 <111434923+nicbritz89@users.noreply.github.com>
2022-10-05 17:18:46 +0200

    Add files via upload

---
+++ W3D3HW.js
@@ -0,0 +1,96 @@
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- You can Google / use StackOverflow BUT only when you think you need something we didn't cover during lessons yet
+- You can test your code in a separate file or de-commenting the single exercises in this one.
+- You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
+- The solution must be available for the tutors by the end of the day (5PM CET)
+*/
+
+/* EXERCISE 1
+ Enumerate and describe the main datatypes in JavaScript. Try to explain the concepts as you were speaking to a 12 years old.
+*/
+/*
+Number: (i)The number type represents both integer and floating point numbers.
+        (ii)There are many operations for numbers, e.g. multiplication *, division /, addition +, subtraction -,
+
+String:(i)- Are for storing and manipulating text.
+       (ii) Is zero or more characters written inside quotes.
+
+Bolean:(i) Represents one of two values: true or false.
+
+Null: (i) Represents an empty or non existent value.
+
+Undefined: (i) Means a variable has been declared
+           (ii) but the content of that variable has not yet been defined.
+
+     */
+
+/* EXERCISE 2
+ Try to describe what a variable is, in your own words.
+*/
+
+/* A variable is a container that stores data and is declared with let keyword */
+
+/* EXERCISE 3
+ Write the code to execute an addition (a sum) of the numbers 12 and 20.
+*/
+
+let number1 = 12;
+let number2 = 20;
+let numbersum = number1 + number2;
+console.log("Exercise 3: the answer given to me is...." + numbersum)
+
+
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 4
+ Create a variable named x and assign to it the number 12.
+*/
+let x = 12;
+console.log("Exercise 4: X is equal to " + x)
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 5
+ Create a variable called name and assign to it the string "John Doe".
+*/
+let name = "John Doe";
+console.log("Exercise 5: His name is... " +  name)
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 6
+ Execute a subtraction between the number 12 and the variable x you declared before (which is storing the value 12).
+*/
+let number6 = 12;
+let numbersume6 = number6 - x;
+console.log("Exercise 6: the answer given to me is...." + numbersume6)
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 7
+ Create two variables: name1 and name2. Assign to name1 the string "john", and to name2 the string "John" (this one has a capital J!)
+ Verify that name1 is different from name2 (HINT: check their equality to be false).
+ Finally, verify that the equality between name1 and name2 becomes true if both are lowercase (without changing the value of name2).
+*/
+let name1 = "john"
+let name2 = "John"
+(name1 === name2)
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 8
+ Create a variable named x, and assign to it a numeric value less than 10.
+ Write the code to print the literal value of x (ex.: if x is 1, it should print "one", if x is 5 it should print "five" etc.).
+*/
+
+
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 9
+ [Extra] Assign a value to a variable using a ternary operator (this topic has not been covered during lesson yet,
+ try to search it by yourself, tomorrow morning we'll discuss it together!)
+*/
+
+/* WRITE YOUR ANSWER HERE */
